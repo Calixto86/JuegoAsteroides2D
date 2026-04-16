82067a141cd1387f8f372a83873e46eda306181c
calixto86 <calixtomamani972@gmail.com>
2026-04-15 21:38:15 -0400

    feat: nave con disparos

--- game.js
+++ game.js
@@ -1,19 +1,26 @@
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
 
-
+const COLORES = {
+    nave: "#00ffff",
+    bala: "#facc15",
+    estrellas: "#ffffff",
+    borde: "#1e3a5f"
+};
 
 function dibujarMarco() {
-    ctx.strokeStyle = "#1e3a5f";
+    ctx.strokeStyle = COLORES.borde;
     ctx.lineWidth = 2;
     ctx.strokeRect(0, 0, canvas.width, canvas.height);
 }
 
 const estrellas = [];
-const keys = {};//guarda el estado del teclado
+const keys = {}; 
+
 document.addEventListener("keydown", e => keys[e.key] = true);
 document.addEventListener("keyup", e => keys[e.key] = false);
 
+const balas = [];
 
 function crearEstrellas() {
     for (let i = 0; i < 100; i++) {
@@ -26,7 +33,7 @@ function crearEstrellas() {
 }
 
 function dibujarEstrellas() {
-    ctx.fillStyle = "white";
+    ctx.fillStyle = COLORES.estrellas;
 
     estrellas.forEach(e => {
         ctx.beginPath();
@@ -35,74 +42,119 @@ function dibujarEstrellas() {
     });
 }
 
-function Ship(x, y) {
+function Nave(x, y) {
     this.x = x;
     this.y = y;
-    this.angle = 0;
-    this.velocityX = 0;
-    this.velocityY = 0;
-
-
+    this.angulo = 0;
+    this.velocidadX = 0;
+    this.velocidadY = 0;
+    this.cooldownDisparo = 0;
 
     this.draw = function () {
         ctx.save();
         ctx.translate(this.x, this.y);
-        ctx.rotate(this.angle);
+        ctx.rotate(this.angulo);
+
         ctx.beginPath();
         ctx.moveTo(0, -20);
         ctx.lineTo(-15, 15);
         ctx.lineTo(15, 15);
         ctx.closePath();
-        ctx.strokeStyle = "#00ffff";
+
+        ctx.strokeStyle = COLORES.nave;
         ctx.stroke();
+
         ctx.restore();
     }
 
     this.update = function () {
         if (keys["ArrowLeft"]) {
-            this.angle -= 0.2;
+            this.angulo -= 0.2;
         }
         if (keys["ArrowRight"]) {
-            this.angle += 0.2;
+            this.angulo += 0.2;
         }
-        //acelera hacia adelante
+
+        // acelerar
         if (keys["ArrowUp"]) {
-            this.velocityX += Math.cos(this.angle - Math.PI / 2) * 0.1;
-            this.velocityY += Math.sin(this.angle - Math.PI / 2) * 0.1;
+            this.velocidadX += Math.cos(this.angulo - Math.PI / 2) * 0.1;
+            this.velocidadY += Math.sin(this.angulo - Math.PI / 2) * 0.1;
         }
-        //aplicamos moviemiento
-        this.x += this.velocityX;
-        this.y += this.velocityY;
-        this.velocityX *= 0.98;
-        this.velocityY *= 0.98;
 
-        // Controlar que no sobrepase los limites
-        if (this.x > canvas.width)
-            this.x = 0;
-        if (this.x < 0)
-            this.x = canvas.width;
-        if (this.y > canvas.height)
-            this.y = 0;
-        if (this.y < 0)
-            this.y = canvas.height;
+        this.x += this.velocidadX;
+        this.y += this.velocidadY;
 
+        this.velocidadX *= 0.98;
+        this.velocidadY *= 0.98;
+
+        // límites (wrap)
+        if (this.x > canvas.width) this.x = 0;
+        if (this.x < 0) this.x = canvas.width;
+        if (this.y > canvas.height) this.y = 0;
+        if (this.y < 0) this.y = canvas.height;
+
+        if (this.cooldownDisparo > 0) {
+            this.cooldownDisparo--;
+        }
+
+        // disparo
+        if (keys[" "] && this.cooldownDisparo <= 0) {
+
+            let offsetX = Math.cos(this.angulo - Math.PI / 2) * 20;
+            let offsetY = Math.sin(this.angulo - Math.PI / 2) * 20;
+
+            balas.push(
+                new Bala(
+                    this.x + offsetX,
+                    this.y + offsetY,
+                    this.angulo
+                )
+            );
+
+            this.cooldownDisparo = 10;
+        }
     }
 }
 
-let ship = new Ship(canvas.width / 2, canvas.height / 2);
+function Bala(x, y, angulo) {
+    this.x = x;
+    this.y = y;
+
+    this.velocidadX = Math.cos(angulo - Math.PI / 2) * 8;
+    this.velocidadY = Math.sin(angulo - Math.PI / 2) * 8;
+
+    this.draw = function () {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, 3, 0, Math.PI * 2);
+        ctx.fillStyle = COLORES.bala;
+        ctx.fill();
+    }
+
+    this.update = function () {
+        this.x += this.velocidadX;
+        this.y += this.velocidadY;
+    }
+}
+
+let nave = new Nave(canvas.width / 2, canvas.height / 2);
 
 function loop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ship.update();
+
+    nave.update();
 
     dibujarEstrellas();
     dibujarMarco();
-    ship.draw();
+
+    balas.forEach(bala => {
+        bala.update();
+        bala.draw();
+    });
+
+    nave.draw();
 
     requestAnimationFrame(loop);
 }
+
 crearEstrellas();
 loop();
-
-
-
